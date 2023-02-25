c8cc775fb21baa0f96b24703522999ec51b5a045
arampa01 <arampa01@student.ubc.ca>
2023-02-25 15:28:21 -0800

    Finish tests part 2 Currently 100% on code coverage on Intelli-J

--- src/test/model/ListOfEntriesTest.java
+++ src/test/model/ListOfEntriesTest.java
@@ -10,16 +10,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ListOfEntriesTest {
     private ListOfEntries testList;
     private ArrayList<Entry> entryArrayList;
-    private Entry e;
+    private Entry e1;
+    private Entry e2;
 
     @BeforeEach
     void runBefore() {
         testList = new ListOfEntries();
         entryArrayList = new ArrayList<>();
-        e = new Entry("Bing", "a@outlook.com", new Password("helloworld123"),
+        e1 = new Entry("Bing", "a@outlook.com", new Password("helloworld123"),
                 "www.bing.ca", "none");
+        e2 = new Entry("Adobe", "a@outlook.com", new Password("goodpassword"),
+                "www.adobe.ca", "none");
 
-        entryArrayList.add(e);
+        entryArrayList.add(e1);
+        entryArrayList.add(e2);
     }
 
     @Test
@@ -32,7 +36,30 @@ public class ListOfEntriesTest {
         testList = new ListOfEntries(entryArrayList);
 
         ArrayList<Entry> actualList = testList.getEntries();
-        assertEquals(1, actualList.size());
-        assertEquals(e, actualList.get(0));
+        assertEquals(2, actualList.size());
+        assertEquals(e1, actualList.get(0));
+        assertEquals(e2, actualList.get(1));
+    }
+
+    @Test
+    void testGetEntryAtIndexZero() {
+        testList = new ListOfEntries(entryArrayList);
+
+        assertEquals(e1, testList.getEntryAtIndex(0));
+    }
+
+    @Test
+    void testGetEntryAtIndexLast() {
+        testList = new ListOfEntries(entryArrayList);
+
+        assertEquals(e2, testList.getEntryAtIndex(1));
+    }
+
+    @Test
+    void testAddEntry() {
+        testList.addEntry(e1);
+
+        assertEquals(testList.getSizeOfEntries(), 1);
+        assertEquals(testList.getEntryAtIndex(0), e1);
     }
 }
