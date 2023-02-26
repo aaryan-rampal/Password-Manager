60b30db6c4e26bf4e9ba61fca12b64b17cac88c1
arampa01 <arampa01@student.ubc.ca>
2023-02-26 14:11:38 -0800

    Remove code to compile

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,8 +3,6 @@ package ui;
 public class Main {
 
     public static void main(String[] args) {
-        PasswordManager passwordManager = new PasswordManager();
-        passwordManager.start();
     }
 
 }
