d702dffd83d34e327913ebc55c22fdea9d4c3156
arampa01 <arampa01@student.ubc.ca>
2023-02-26 20:36:33 -0800

    Finish task 1

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,9 +3,6 @@ package ui;
 public class Main {
 
     public static void main(String[] args) {
-        PasswordManager pm = new
-                PasswordManager();
-        pm.start();
     }
 
 }
