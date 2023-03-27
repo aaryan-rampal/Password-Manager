17fed54e20cc896a4d1dbc7acfb56dc2c299e31d
arampa01 <arampa01@student.ubc.ca>
2023-03-27 15:02:47 -0700

    Changes before GUI TODO: finish ByteConvertorTest.java

--- src/test/model/ByteConvertorTest.java
+++ src/test/model/ByteConvertorTest.java
@@ -3,7 +3,10 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ByteConvertorTest {
 
@@ -16,8 +19,18 @@ public class ByteConvertorTest {
 
     @Test
     void testStringToBytes() {
-        String test = "blah";
-        byte[] testBytes = bc.stringToBytes(test);
-        assertEquals(testBytes, bc.stringToBytes(test));
+        String test = "testing";
+        byte[] testBytes = {(byte) 0xB5, (byte) 0xEB, 0x2D, (byte) 0x8A, 0x78};
+        assertTrue(Arrays.equals(bc.stringToBytes(test), testBytes));
     }
+
+//    @Test
+//    void testBytesToString() {
+//        byte[] testBytes = {(byte) 0xDB, (byte) 0x5D, (byte) 0x29, (byte) 0xAE, (byte) 0x88, (byte) 0xDE, (byte) 0x72};
+//        String test = "210project";
+//        byte[] aaa = bc.stringToBytes(test);
+//        String a = bc.bytesToString(aaa);
+//        System.out.println("");
+//        assertEquals(bc.bytesToString(testBytes), test);
+//    }
 }
