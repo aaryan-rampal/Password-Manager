c8c42d6db98f733cc00051758c592a2330f8d291
arampa01 <arampa01@student.ubc.ca>
2023-03-30 21:09:03 -0700

    Improve testReaderGeneralWorkRoom test

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -45,9 +45,14 @@ class JsonReaderTest extends JsonTest {
             List<Entry> entries = file.getEntries();
 
             assertEquals(2, entries.size());
-            checkEntry("Google", "a@gmail.com",
-                    new Password("password123"), "www.google.ca",
-                    "none", entries.get(0));
+            assertEquals(entries.get(0).getName(), "Google");
+            assertEquals(entries.get(0).getUsername(), "a@gmail.com");
+            assertEquals(entries.get(0).getPassword().getPassword(), "password123");
+            assertEquals(entries.get(0).getUrl(), "www.google.ca");
+            assertEquals(entries.get(0).getNotes(), "none");
+//            checkEntry("Google", "a@gmail.com",
+//                    new Password("password123"), "www.google.ca",
+//                    "none", entries.get(0));
             checkEntry("Bing", "b@outlook.com",
                     new Password("engaging-iodine-quicksand-canola"), "www.bing.ca", "none",
                     entries.get(1));
