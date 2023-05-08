e8e103e1913f2d081606dcf57f51d15f36262018
aaryan-rampal <76913929+aaryan-rampal@users.noreply.github.com>
2023-05-07 21:41:49 -0500

    Delete unused Writable

--- src/main/persistence/Writable.java
+++
@@ -1,8 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// Interface for classes that are meant to be converted to a JSON object/array
-public interface Writable {
-    JSONObject toJson();
-}
